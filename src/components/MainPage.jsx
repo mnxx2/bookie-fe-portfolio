ff331f37e40e3c2a47cab94de83c7798ad40cd42
mnxx2 <alsdk4931@gmail.com>
2025-10-14 02:45:19 +0900

    사용하지 않는 import 삭제, CI=false 추가

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
 import { getBookShelves } from "./BooksApi";
 import BookShelvesCard from "./BookShelvesCard";
 
